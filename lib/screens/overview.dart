d54eb9f66c3e4c3608aac5a8ac032f96424a54be
ademie <isijolaademie@gmail.com>
2023-03-17 15:28:45 -0600

    refactored default tab controller

--- lib/screens/overview.dart
+++ lib/screens/overview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:writeapp/screens/overviewwidgets/search_bar.dart';
 import 'package:writeapp/screens/overviewwidgets/tab_contents.dart';
+import 'package:writeapp/screens/overviewwidgets/tab_controller.dart';
 import 'package:writeapp/screens/overviewwidgets/top_bar.dart';
 import 'package:writeapp/theme/colors.dart';
 
@@ -34,32 +35,7 @@ class _OverviewState extends State<Overview> {
                     SearchBar(),
                     DefaultTabController(
                       length: 2,
-                      child: Column(
-                        children: [
-                          TabBar(
-                            indicatorColor: WriteColors.primary,
-                            unselectedLabelStyle: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w500),
-                            labelStyle: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w800),
-                            tabs: [
-                              Tab(
-                                text: 'All Notes',
-                              ),
-                              Tab(
-                                text: 'Folders',
-                              )
-                            ],
-                          ),
-                          Container(
-                              height: MediaQuery.of(context).size.height * 0.8,
-                              padding: EdgeInsets.only(bottom: 100),
-                              child: TabBarView(children: [
-                                TabContents(),
-                                TabContents(),
-                              ]))
-                        ],
-                      ),
+                      child: TabControllerChild(),
                     ),
                   ],
                 ),
@@ -71,3 +47,4 @@ class _OverviewState extends State<Overview> {
     );
   }
 }
+
---
+++ lib/screens/overviewwidgets/tab_controller.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:writeapp/screens/overviewwidgets/tab_contents.dart';
+import 'package:writeapp/theme/colors.dart';
+
+class TabControllerChild extends StatelessWidget {
+  const TabControllerChild({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TabBar(
+          indicatorColor: WriteColors.primary,
+          unselectedLabelStyle: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.w500),
+          labelStyle: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.w800),
+          tabs: [
+            Tab(
+              text: 'All Notes',
+            ),
+            Tab(
+              text: 'Folders',
+            )
+          ],
+        ),
+        Container(
+            height: MediaQuery.of(context).size.height * 0.8,
+            padding: EdgeInsets.only(bottom: 100),
+            child: TabBarView(children: [
+              TabContents(),
+              TabContents(),
+            ]))
+      ],
+    );
+  }
+}
