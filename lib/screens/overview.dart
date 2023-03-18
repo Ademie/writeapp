7b6f422ec193971f080dac5b0ebb497fb3f55f85
ademie <isijolaademie@gmail.com>
2023-03-18 02:44:47 -0600

    added BottomBar as bottomSheet

--- lib/screens/overview.dart
+++ lib/screens/overview.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:writeapp/screens/overviewwidgets/bottom_bar.dart';
+
 import 'package:writeapp/screens/overviewwidgets/search_bar.dart';
 import 'package:writeapp/screens/overviewwidgets/tab_controller.dart';
 import 'package:writeapp/screens/overviewwidgets/top_bar.dart';
-import 'package:writeapp/theme/colors.dart';
+
 
 class Overview extends StatefulWidget {
   const Overview({super.key});
@@ -33,26 +34,10 @@ class _OverviewState extends State<Overview> {
                 ],
               ),
             ),
-            // BottomBar()
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            label: 'First',
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.mail),
-            label: 'Second',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Thired',
-          ),
-        ],
-      ),
+      bottomSheet: BottomBar(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
